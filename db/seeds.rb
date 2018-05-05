490e8f0c8859c3566c11be80b8722997ead5c448
trifiomil <trifiomil@hotmail.com>
2018-05-05 12:39:00 -0500

    Crear Usuario Tristan en el seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  name: "Alfonse",
+  last_name_p: "Elric",
+  last_name_m: Null,
+  mail: "aelric@alch.net",
+  telephone: "123457890",
+  birth_date: "1995-05-04",
+  passport: "34893"
+)
