d36e94ef9e0291d350047abcf800fcc340224acb
eduAguilar96 <eduxx77@gmail.com>
2018-05-06 04:48:14 -0500

    Fix Users#create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@ class User < ApplicationRecord
   has_many :reservation_informations
   has_many :tickets
 
+
+  validates :name, presence: true
+  validates :last_name_p, presence: true
+  validates :last_name_m, presence: true
   validates :mail, uniqueness: true,
-    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :passport, uniqueness: true
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    presence: true
+  validates :telephone, presence: true
+  validates :birth_date, presence: true
+  validates :passport, uniqueness: true, presence: true
 
 end
