ebd923546aa95a16073024f84e874e03cbbfc172
eduAguilar96 <eduxx77@gmail.com>
2018-05-05 22:05:13 -0500

    Create queries for Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,6 +4,34 @@ class Flight < ApplicationRecord
 
   has_many :reservation_informations
 
+  def self.todos
+    ActiveRecord::Base.connection.exec_query(
+      'SELECT '\
+        '* '\
+      'FROM flights'
+    )
+  end
+
+  def self.algunos(origin, destination, from, to)
+    ActiveRecord::Base.connection.exec_query(
+      'SELECT '\
+        '* '\
+      'FROM flights '\
+      "WHERE flights.origin = '#{origin}' "\
+      "AND flights.destination = '#{destination}' "\
+      "AND flights.day BETWEEN '#{from}' AND '#{to}'"
+    )
+  end
+
+  def self.uno(id)
+    ActiveRecord::Base.connection.exec_query(
+      'SELECT '\
+        '* '\
+      'FROM flights '\
+      "WHERE flights.id = '#{id}'"
+    )
+  end
+
   private
 
   def generate_id
