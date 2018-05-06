f2cdeb8b68ac24b644ccc097fac265dcb89a1a5e
eduAguilar96 <eduxx77@gmail.com>
2018-05-06 02:58:55 -0500

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :reservation_informations
   has_many :tickets
 
+  validates :mail, uniqueness: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :passport, uniqueness: true
+
 end
