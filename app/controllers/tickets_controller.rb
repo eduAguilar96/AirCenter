922e3a81a0230246ec30d85741f1056773f63736
eduAguilar96 <eduxx77@gmail.com>
2018-05-06 20:48:58 -0500

    Create Ticket#create

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,11 @@
 class TicketsController < ApplicationController
   def create
+    @reservation = Reservation.find_by(id: params[:reservation_id])
+    if @tickets = Ticket.create(create_params)
+      redirect_to @reservation
+    else
+      redirect_to @reservation
+    end
   end
 
   def index
@@ -12,5 +18,22 @@ class TicketsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def create_params
+    params[:tickets].each do |ticket|
+      ticket[:cost] = params[:cost]
+    end
+    params.permit(tickets:
+      [
+        :reservation_id,
+        :user_id,
+        :seat,
+        :zone,
+        :cost
+      ]
+    )[:tickets]
   end
 end
